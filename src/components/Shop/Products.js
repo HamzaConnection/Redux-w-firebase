7dd79fb7f13d4a5d6e4f16278fd8f821042cbfbd
Hamza Laroussi <hamza.laroussi@capgemini.com>
2022-11-16 12:32:15 +0100

    test firebase

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -5,13 +5,13 @@ const DUMMY_PRODUCTS = [
   {
     id: 'p1',
     price: 6,
-    title: 'My First Book',
+    title: 'Hamzas First Book',
     description: 'The first book I ever wrote',
   },
   {
     id: 'p2',
     price: 5,
-    title: 'My Second Book',
+    title: 'Hamzs Second Book',
     description: 'The second book I ever wrote',
   },
 ];
